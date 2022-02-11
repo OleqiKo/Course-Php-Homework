f3b7e01a68754b5588b789bba8bc84deea489e27
Oleg <oleq.korzhit@gmail.com>
2022-02-12 01:04:50 +0200

    add new homework

---
+++ Lesson3(homework)/oddEven.php
@@ -0,0 +1,8 @@
+<?php
+$randValue = rand(0, 100);
+
+if($randValue % 2 === 0) {
+    echo 'even';
+} else {
+    echo  'odd';
+}
---
+++ Lesson3(homework)/randSwitch.php
@@ -0,0 +1,48 @@
+<?php
+$randValue = rand(0, 50);
+
+switch ($randValue) {
+    case 1: {
+        echo "odd";
+        break;
+    }
+    case 2: {
+        echo "odd";
+        break;
+    }
+    case 3: {
+        echo "odd";
+        break;
+    }
+    case 4: {
+        echo "odd";
+        break;
+    }
+    case 5: {
+        echo "odd";
+        break;
+    }
+    case 10: {
+        echo "even";
+        break;
+    }
+    case 20: {
+        echo "even";
+        break;
+    }
+    case 30: {
+        echo "even";
+        break;
+    }
+    case 40: {
+        echo "even";
+        break;
+    }
+    case 50: {
+        echo "even";
+        break;
+    }
+    default: {
+        echo "Oops";
+    }
+}
